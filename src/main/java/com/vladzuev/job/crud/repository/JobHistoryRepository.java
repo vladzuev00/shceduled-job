cea1b04c95a098d9978a3c6002ef05cbdfab941e
Vlad Zuev <vladzuev.00@mail.ru>
2023-12-04 14:29:37 +0300

    add JobHistoryRepository

---
+++ src/main/java/com/vladzuev/job/crud/repository/JobHistoryRepository.java
@@ -0,0 +1,8 @@
+package com.vladzuev.job.crud.repository;
+
+import com.vladzuev.job.crud.entity.JobHistoryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobHistoryRepository extends JpaRepository<Long, JobHistoryEntity> {
+
+}
