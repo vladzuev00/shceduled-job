7485c577718e1f4b94d3738c891067562abde32e
Vlad Zuev <vladzuev.00@mail.ru>
2023-12-06 05:00:00 +0300

    refactor ForeverPrintingMessageScheduledTask

--- src/main/java/com/vladzuev/schedulingtask/model/task/foreverrepeated/ForeverPrintingMessageScheduledTask.java
+++ src/main/java/com/vladzuev/schedulingtask/model/task/foreverrepeated/ForeverPrintingMessageScheduledTask.java
@@ -1,6 +1,5 @@
 package com.vladzuev.schedulingtask.model.task.foreverrepeated;
 
-import com.vladzuev.schedulingtask.crud.dto.User;
 import com.vladzuev.schedulingtask.model.ScheduledTaskRunInterval;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -14,12 +13,10 @@ import java.time.Instant;
 public final class ForeverPrintingMessageScheduledTask extends ForeverRepeatedScheduledTask {
     private final String message;
 
-    public ForeverPrintingMessageScheduledTask(final Long id,
-                                               final Instant startDateTime,
-                                               final User user,
+    public ForeverPrintingMessageScheduledTask(final Instant startDateTime,
                                                final ScheduledTaskRunInterval runInterval,
                                                final String message) {
-        super(id, startDateTime, user, runInterval);
+        super(startDateTime, runInterval);
         this.message = message;
     }
 
